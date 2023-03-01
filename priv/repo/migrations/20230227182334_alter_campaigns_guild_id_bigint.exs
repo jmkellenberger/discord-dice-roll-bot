b1f16023dd1fbd187ad7362640bdc8affcf83482
Jack Kellenberger <jackson@kellenberger.io>
2023-03-01 09:19:53 -0500

    Delete initial models

--- priv/repo/migrations/20230227182334_alter_campaigns_guild_id_bigint.exs
+++
@@ -1,9 +0,0 @@
-defmodule Witchspace.Repo.Migrations.AlterCampaignsGuildIdBigint do
-  use Ecto.Migration
-
-  def change do
-    alter table(:campaigns) do
-      modify :guild_id, :bigint
-    end
-  end
-end
